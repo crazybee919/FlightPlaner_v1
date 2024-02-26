d5f17512de717f668159134bb56cd5f27a5f3ad2
crazybee919 <crazybee919@gmail.com>
2024-02-26 10:50:11 +0200

    Empty FP

--- Controllers/CleanupApiController.cs
+++ Controllers/CleanupApiController.cs
@@ -1,18 +1,1 @@
-﻿using FlightPlaner.Storage;
-using Microsoft.AspNetCore.Mvc;
-
-namespace FlightPlaner.Controllers
-{
-    [Route("testing-api")]
-    [ApiController]
-    public class CleanupApiController : ControllerBase
-    {
-        [HttpPost]
-        [Route("clear")]
-        public IActionResult Clear()
-        {
-            FlightStorage.Clear();
-            return Ok();
-        }
-    }
-}
+﻿
